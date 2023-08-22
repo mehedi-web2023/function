72050c55d87031a0c47af9bdfaaf801ea04457a8
mehedi-web2023 <courseweb38@gmail.com>
2023-08-22 19:31:18 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,103 @@
+
+// var count = []
+
+// var i = 0;
+
+// while(i<=100){
+
+//     count.push(i)
+//     i = i +2
+// }
+
+// console.log(count)
+
+
+// var arr = [2,3,4]
+
+// arr.shift()
+// arr.unshift(1)
+
+// console.log(arr)
+
+
+
+// var student = {
+//     name:"sanjida",
+//     age:120,
+//     class:"vi",
+//     address:"narayanganj",
+//     roll:10
+// }
+
+// console.log(student.age)
+
+
+// var age= [2,4,6,7,8,9,]
+// var istrue = age.includes(22)
+// console.log(age.includes(22))
+
+
+// var age= [2,4,6,7,8,9]
+
+// var istrue = age.slice(0,3)
+
+// console.log(istrue)
+
+
+
+// let num1 = 10
+
+// num1 = 20
+
+// console.log(num1)
+
+// const num1 = 10
+
+// num1 = 20
+
+// console.log(num1)
+
+
+function sanjida (num,num1,sanjida){
+
+    const singara = 10;
+    const puri = 10;
+    totalkhors = singara +puri
+    
+    let total = num+num1+sanjida
+
+    roilo = total - totalkhors
+    
+    return roilo
+}
+
+let adiba = sanjida(10,20,30)
+console.log(adiba)
+
+
+
+
+
+// arrow function
+const nus_00 = () =>{
+
+}
+
+
+
+const bangla =22;
+
+
+if(bangla>=80){
+    console.log("A+")
+}
+ else if (bangla>=70){
+    console.log("A")
+
+ }
+
+
+
+
+
+
